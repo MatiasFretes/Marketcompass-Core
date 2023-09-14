f26e07601e4d70a6c719d7f13b16815aaee915e7
unknown <maty.fretes10@gmail.com>
2023-09-13 21:21:28 -0300

    Implementacion de discovery para JAR

--- lib/src/main/java/extensible/BuscadorCriterios.java
+++ lib/src/main/java/extensible/BuscadorCriterios.java
@@ -2,13 +2,53 @@ package extensible;
 
 import java.util.Set;
 
+import java.io.File;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+
 public class BuscadorCriterios {
 	
-	public Set<FiltradorPorCriterio> buscar(){
-		
-		
-		
-		return null;
-	}
+    @SuppressWarnings("resource")
+	Set<FiltradorPorCriterio> buscar(String path) {
+        Set<FiltradorPorCriterio> result = new HashSet<>();
+        try {
+            File jarFile = new File(path);
 
+            URL url = jarFile.toURI().toURL();
+            URLClassLoader classLoader = new URLClassLoader(new URL[]{url});
+
+            JarFile jar = new JarFile(jarFile);
+            for (JarEntry entry : Collections.list(jar.entries())) {
+            	if (!entry.getName().endsWith(".class")) continue;
+
+                String className = entry.getName().replace("/", ".").replace(".class", "");
+                Class<?> cls = classLoader.loadClass(className);
+
+                if(!FiltradorPorCriterio.class.isAssignableFrom(cls))
+                   throw new RuntimeException("La clase encontrada no es asignable a un FiltradorPorCriterio");
+
+                FiltradorPorCriterio criterioEncontrado = (FiltradorPorCriterio) cls.getDeclaredConstructor().newInstance();
+                result.add(criterioEncontrado);
+            }
+            classLoader.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+//    public static void main(String[] args) {
+//    	BuscadorCriterios discovery = new BuscadorCriterios();
+//        Set<FiltradorPorCriterio> classes = discovery.buscar(".../lib.jar");
+//
+//        for (FiltradorPorCriterio obj : classes) {
+//        	FiltradorPorCriterio criterio = (FiltradorPorCriterio) obj;
+//            String message = criterio.filtrar();
+//            System.out.println(message);
+//        }
+//    }
 }
