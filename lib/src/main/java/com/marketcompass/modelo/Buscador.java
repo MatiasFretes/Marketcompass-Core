5e589c6c7bfff65f5f066c66aa5c047aa33232a4
David Helguero <dahelguero@gmail.com>
2023-09-15 01:55:30 -0300

    logica core

--- lib/src/main/java/com/marketcompass/modelo/Buscador.java
+++ lib/src/main/java/com/marketcompass/modelo/Buscador.java
@@ -1,41 +1,54 @@
 package com.marketcompass.modelo;
 
+import extensible.BuscadorCriterios;
 import extensible.FiltradorPorCriterio;
 import org.apache.commons.math3.util.Pair;
 import java.io.*;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
+
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Buscador {
 
-    FiltradorPorCriterio filtradorPorCriterio;
-
-    public Buscador(FiltradorPorCriterio filtradorPorCriterio) {
-        this.filtradorPorCriterio = filtradorPorCriterio;
+    public Buscador() {
     }
 
     Pair<String, List<String>> buscar(List<String> productos){
         Map<String, List<String>> mercados = new HashMap<>();
-
-        
-        return filtradorPorCriterio.filtrar(mercados);
-    }
-
-    public static void main(String[] args) {
         ObjectMapper mapper = new ObjectMapper();
-        Map<String, List<String>> mercados = new HashMap<>();
-        String ruta = "mercados/mercados.json";
-        
+        String ruta = "C:\\Users\\Usuario\\IdeaProjects\\MultiMarket-PP2-Core\\lib\\mercados\\mercados.json";
+        BuscadorCriterios buscadorCriterios = new BuscadorCriterios();
         try {
-            mercados = mapper.readValue(new File(ruta), new TypeReference<Map<String, List<String>>>(){});
-            System.out.print(mercados);
-        } catch (IOException e) {
+            mercados = mapper.readValue(new File(ruta), new TypeReference<>() {});
+            Set<FiltradorPorCriterio> f = buscadorCriterios.buscar("C:\\Users\\Usuario\\Downloads\\lib.jar");
+            FiltradorPorCriterio ff = f.stream().findFirst().get();
+            mercados = obtenerMercadosPorProductos(mercados, productos);
+            return ff.filtrar(mercados);
+
+        }
+        catch (IOException e) {
             e.printStackTrace();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
 
+
+        return null;
+    }
+
+    private Map<String, List<String>> obtenerMercadosPorProductos(Map<String, List<String>> mercados, List<String> productos){
+        return mercados.entrySet().stream().filter(mercado -> mercado.getValue().containsAll(productos)).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+    public static void main(String[] args) throws Exception {
+
+        Buscador buscador = new Buscador();
+        List<String> productos = List.of("Manzanas", "Arroz", "Camisetas");
+        Pair<String, List<String>> x = buscador.buscar(productos);
+
+
+        System.out.print("");
     }
    
 }
--- lib/src/main/java/com/marketcompass/modelo/Recomendador.java
+++ lib/src/main/java/com/marketcompass/modelo/Recomendador.java
@@ -2,13 +2,14 @@ package com.marketcompass.modelo;
 
 import org.apache.commons.math3.util.Pair;
 import java.util.List;
+import java.util.Map;
 
 public class Recomendador {
 
     Buscador buscador;
 
-    public Recomendador(Buscador buscador) {
-        this.buscador = buscador;
+    public Recomendador() {
+        this.buscador = new Buscador();
     }
 
     Pair<String, List<String>> recomendar(List<String> productos){
