134a54874679ae790fe0ceb60c5bcae5d1590c3d
melinaze <melinazelaya99@gmail.com>
2023-10-16 17:34:46 -0300

    se unifica ca2 y ca3 de test user4

--- lib/src/test/java/US4.java
+++ lib/src/test/java/US4.java
@@ -14,53 +14,47 @@ import modelo.CoreInit;
 import modelo.Recomendacion;
 
 class US4 {
-	private CoreInit coreInit;
-	private Core core;
-	private String rutaJsonMercados = "src/test/resources/mercados.json";
-	private String rutaJarCriterio = "src/test/resources/DistanciasMultiples.jar";
-	private List<String> productoExistente = Arrays.asList("P3");
-	private String mercadoEsperadoCercano = "M1";
-	private String mercadoEsperadoLejano = "M2";
-	private FiltradorPorCriterio criterioDistanciaCercana;
-	private FiltradorPorCriterio criterioDistanciaLejana;
+    private CoreInit coreInit;
+    private Core core;
+    private String rutaJsonMercados = "src/test/resources/mercados.json";
+    private String rutaJarCriterio = "src/test/resources/DistanciasMultiples.jar";
+    private List<String> productoExistente = Arrays.asList("P3");
+    private String mercadoEsperadoCercano = "M1";
+    private String mercadoEsperadoLejano = "M2";
+    private FiltradorPorCriterio criterioDistanciaCercana;
+    private FiltradorPorCriterio criterioDistanciaLejana;
+    private Set<FiltradorPorCriterio> criterios;
 
-	@BeforeEach 
-	public void setup() {
-		CoreInit.RUTA_JSON_MERCADOS = rutaJsonMercados;
-		CoreInit.RUTA_JAR_CRITERIO = rutaJarCriterio;
-		coreInit = new CoreInit();
-		core = coreInit.inicializar();
-		obtenerCriterios();
-	}
-	
-	@Test
-	public void CA1_CriterioNoSeleccionado() {
-		Recomendacion recomendacion = core.obtenerRecomendacion(null,productoExistente);
-		assertTrue(recomendacion.toString().equals("Lo sentimos, el sistema no ha encontrado ninguna recomendacion de mercado en este momento."));
-	}
-	
-	@Test
-	public void CA2_CriterioDistanciaCercana() {
-		Recomendacion recomendacion = core.obtenerRecomendacion(criterioDistanciaCercana,productoExistente);
-		assertTrue(recomendacion.getMercado().getNombre().equals(mercadoEsperadoCercano));
-	}
+    @BeforeEach
+    public void setup() {
+        CoreInit.RUTA_JSON_MERCADOS = rutaJsonMercados;
+        CoreInit.RUTA_JAR_CRITERIO = rutaJarCriterio;
+        coreInit = new CoreInit();
+        core = coreInit.inicializar();
+        criterios = core.criterios;
+    }
 
-	@Test
-	public void CA3_CriterioDistanciaLejana() {
-		Recomendacion recomendacion = core.obtenerRecomendacion(criterioDistanciaLejana,productoExistente);
-		assertTrue(recomendacion.getMercado().getNombre().equals(mercadoEsperadoLejano));
-	}
-	
-	public void obtenerCriterios() {
-		Set<FiltradorPorCriterio> criterios = core.criterios;
-		List<FiltradorPorCriterio> listaCriterios = new ArrayList<>(criterios);
-		for (int i = 0; i < listaCriterios.size(); i++) {
-			FiltradorPorCriterio criterio = listaCriterios.get(i);
-		    if (i == 0) {
-		    	criterioDistanciaCercana = criterio;
-		    } else if (i == 1) {
-		    	criterioDistanciaLejana = criterio;
-		    }
-		}
-	}
+    @Test
+    public void CA1_CriterioNoSeleccionado() {
+        Recomendacion recomendacion = core.obtenerRecomendacion(null, productoExistente);
+        assertTrue(recomendacion.toString().equals("Lo sentimos, el sistema no ha encontrado ninguna recomendacion de mercado en este momento."));
+    }
+
+  @Test
+   public void CA2_DistanciasMultiples() {	
+       List<FiltradorPorCriterio> listaCriterios = new ArrayList<>(criterios);
+       criterioDistanciaCercana = listaCriterios.get(1);
+       criterioDistanciaLejana = listaCriterios.get(0);
+       
+       Recomendacion recomendacionCercana = core.obtenerRecomendacion(criterioDistanciaCercana, productoExistente);
+       Recomendacion recomendacionLejana = core.obtenerRecomendacion(criterioDistanciaLejana, productoExistente);
+       
+       assertTrue(recomendacionCercana.getMercado().getNombre().equals(mercadoEsperadoCercano));
+       assertTrue(recomendacionLejana.getMercado().getNombre().equals(mercadoEsperadoLejano));
+    }
+    
+
+    
+ 
 }
+
