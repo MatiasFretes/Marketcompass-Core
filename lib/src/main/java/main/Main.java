97024fd101c7dc3a882d320ad49ebece56e76c26
unknown <maty.fretes10@gmail.com>
2023-11-09 16:44:54 -0300

    Se agregan nuevas extensiones con el SeleccionadorPorCriterio

--- lib/src/main/java/main/Main.java
+++ lib/src/main/java/main/Main.java
@@ -1,16 +1,20 @@
 package main;
 
+import java.util.Arrays;
+
 import modelo.Core;
 import modelo.CoreInit;
+import modelo.Recomendacion;
 
 public class Main {
 
 	public static void main(String[] args){
 		CoreInit coreInit = new CoreInit();
 		Core core = coreInit.inicializar();
-		
 		System.out.println(core.obtenerNombresCriterios());
 		
+		Recomendacion recomendacion = core.recomendar(Arrays.asList("P2"));
+		System.out.println(recomendacion.getMercado());
 	}
 
 }
