146fe7b796815638d0f2e4524456a99356b22d63
Matias Fretes <maty.fretes10@gmail.com>
2023-11-10 15:49:37 -0300

    Se elimina archivo properties

--- lib/src/main/java/configuracion/ConfiguracionRutas.java
+++ lib/src/main/java/configuracion/ConfiguracionRutas.java
@@ -1,24 +1,8 @@
 package configuracion;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Properties;
-
 public class ConfiguracionRutas {
 
-    private static Properties properties = new Properties();
-
-    static {
-        try {
-            FileInputStream fis = new FileInputStream("config.properties");
-            properties.load(fis);
-            fis.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
     public static String getRutaExtensionJAR() {
-        return properties.getProperty("rutaExtensionJAR");
+        return "C:\\marketcompass\\Criterios.jar";
     }
 }
